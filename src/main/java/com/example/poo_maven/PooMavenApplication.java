8a06344414b5357a2ffa716a541b1b1fb9bc8a94
laura4343 <123laurafarias@gmail.com>
2024-05-20 12:11:21 -0300

    aaa

---
+++ src/main/java/com/example/poo_maven/PooMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.poo_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PooMavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/poo_maven/PooMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poo_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
